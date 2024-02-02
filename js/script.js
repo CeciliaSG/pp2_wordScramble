89a6392cfc76189f127695bd8d715498913e7af8
CeciliaSG <>
2024-02-02 13:46:26 +0000

    Fix attribute and id for input-field, add word-array to js-file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const wineWords = ['Grape', 'Vineyard', 'Cork', 'Barrel', 'Terroir', 'Tannin', 'Bouquet',
+    'Aroma', 'Merlot', 'Chardonnay', 'Cabernet Sauvignon', 'Syrah', 'Pinot Noir',
+    'Sommelier', 'Decanter', 'Vintage', 'Oak', 'Fermentation', 'Malbec', 'Riesling',
+    'Brut', 'Magnum', 'Sip', 'Swirl', 'Glass', 'Pairing', 'Terroir', 'Zinfandel', 'Reserva', 'Aeration'];
+
+console.log(wineWords[0]);  
