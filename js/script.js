001d81466e2edb2b60f4066a4f8ae8469b302bcb
CeciliaSG <>
2024-02-03 09:02:16 +0000

    Add comment about splice not working

--- js/script.js
+++ js/script.js
@@ -8,12 +8,20 @@ function generateRandomWord(){
    let randomIndex = words[Math.floor(Math.random() * words.length)];
 
     //Remove the used/choosen words from the array so it doesn't load twice
-   //Using the splice method. Searched Stack Overflow for solution
+   //Using the splice method. Searched Stack Overflow for solution. 
+   //Splice not working. Need to find solution.
 
    let randomWord = words.splice(randomIndex, 1)[0];
+
+    console.log(words);
+    console.log("Remaining words:", words);
+    console.log("Number of words after:", words.length);
+
    return randomWord;
 }
 generateRandomWord();
+console.log("Remaining words:", words);
+console.log("Number of words after:", words.length);
 
 
 
