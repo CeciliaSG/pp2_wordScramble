3918b642a52bf5ba642ab826a8a780109918e201
CeciliaSG <>
2024-02-03 09:41:33 +0000

    Add code to function to scramble word and fix issue with scope of randWord, declare it outside the function

--- js/script.js
+++ js/script.js
@@ -1,38 +1,45 @@
 
 // Import words from seperate js-file
-import words from "./words.js"
+import words from "./words.js";
 
-//Add funtion to generate a random word from the word array
+//Add funtion to generate a random word from the words array
 
-function generateRandomWord(){
-   let randomIndex = words[Math.floor(Math.random() * words.length)];
+function generateRandomWord() {
+    let randomIndex = words[Math.floor(Math.random() * words.length)];
 
     //Remove the used/choosen words from the array so it doesn't load twice
-   //Using the splice method. Searched Stack Overflow for solution. 
-   //Splice not working. Need to find solution.
+    //Using the splice method. Searched Stack Overflow for solution. 
+    //Splice not working. Need to find solution. 
+    //Probably has to do with import of words.
 
-   let randomWord = words.splice(randomIndex, 1)[0];
+    let randomWord = words.splice(randomIndex, 1)[0];
 
-    console.log(words);
-    console.log("Remaining words:", words);
-    console.log("Number of words after:", words.length);
-
-   return randomWord;
+    return randomWord;
 }
-generateRandomWord();
-console.log("Remaining words:", words);
-console.log("Number of words after:", words.length);
+let randomWord = generateRandomWord();
 
+//Function to scramble the letters in the word
+//Using split and join, and Fisher-Yates shuffle algorythm
 
+function scrambleWord(word) {
+    let characters = word.split('')
 
-function scrambleWord(){
+    console.log(characters); 
+
+    for (let i = characters.length -1; i > 0; i--){
+        let j = Math.floor(Math.random() * (i+1));
+        [characters[i], characters[j]] = [characters[j], characters[i]];
+    } 
+    return characters.join('');
+}
+
+let scrambled = scrambleWord(randomWord);
+console.log(scrambled);
+
+function startGame() {
 
 }
 
-function startGame(){
+function checkAnswer() {
 
-}
-
-function checkAnswer(){
-    
 }
