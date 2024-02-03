28b6b28ac04223f60b2e958139f687478c5e0804
CeciliaSG <>
2024-02-03 07:28:23 +0000

    Create seperate js file for the word array and add 100 words

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-const wineWords = ['Grape', 'Vineyard', 'Cork', 'Barrel', 'Terroir', 'Tannin', 'Bouquet',
-    'Aroma', 'Merlot', 'Chardonnay', 'Cabernet Sauvignon', 'Syrah', 'Pinot Noir',
-    'Sommelier', 'Decanter', 'Vintage', 'Oak', 'Fermentation', 'Malbec', 'Riesling',
-    'Brut', 'Magnum', 'Sip', 'Swirl', 'Glass', 'Pairing', 'Terroir', 'Zinfandel', 'Reserva', 'Aeration'];
-
-console.log(wineWords[0]);  
-
 function generateRandomWord(){
 
 }
