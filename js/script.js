f53d8527ed3f090dee835b465f4b09f7366d7563
CeciliaSG <>
2024-02-03 08:05:38 +0000

    Add code to randomly choose a random word from the words array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 
+// Import words from seperate js-file
 import words from "./words.js"
-console.log(words[0]);
+
+let words=[];
+
+//Add funtion to gnerate a random word from the word array
 
 function generateRandomWord(){
-
+    return words[Math.floor(Math.random() * words.length)];
+    
+    console.log(generateRandomWord());
 }
 
 function scrambleWord(){
