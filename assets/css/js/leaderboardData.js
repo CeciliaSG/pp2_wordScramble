fcded47562d6ae22b015288850b432511cd60bbd
CeciliaSG <>
2024-02-05 07:30:01 +0000

    Change name leaderboard.js to leaderboardData, and add an array to the file

---
+++ assets/css/js/leaderboardData.js
@@ -0,0 +1,7 @@
+let leaderboard = [
+    { name: "Player1", score: "/100" },
+    { name: "Player2", score: "/100" },
+    { name: "Player3", score: "/100" },
+    { name: "Player4", score: "/100" },
+];
+export {leaderboard};
